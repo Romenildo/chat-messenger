0549ebd9050c5691c41ad17e33abcc9717dc0195
Romenildo Ferreira <romenildoferreira@gmaill.com>
2023-06-01 16:26:10 -0300

    finalizacao visual do formulario

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -10,6 +10,9 @@ import Input from "@/app/components/inputs/Input"
 
 import { useCallback, useState } from "react"
 import { useForm, FieldValues, SubmitHandler } from "react-hook-form"
+import { BsGoogle} from 'react-icons/bs'
+
+import AuthSocialButton from "./AuthSocialButton"
 
 type TypeForm = 'LOGIN' | 'REGISTER'
 
@@ -67,10 +70,10 @@ const AuthForm = ()=>{
 
                     {/* Name so vai aparecer see for para rgistrar um novo usuario */}
                     {typeForm ==="REGISTER" && (
-                        <Input id="name" label="name" register={register} errors={errors} />
+                        <Input id="name" label="Nome" register={register} errors={errors} />
                     )}
-                    <Input id="email" label="Email address" type="email" register={register} errors={errors} />
-                    <Input id="password" label="Password" type="password" register={register} errors={errors} />
+                    <Input id="email" label="Email" type="email" register={register} errors={errors} />
+                    <Input id="password" label="Senha" type="password" register={register} errors={errors} />
 
                     <div>
                         <Button 
@@ -81,6 +84,37 @@ const AuthForm = ()=>{
                         >{typeForm ==='LOGIN'? "Login": "Registrar"}</Button>
                     </div>
                 </form>
+
+                <div className="mt-6">
+
+                    <div className="relative">
+                        {/*Linha cinza no fundo */}
+                        <div className="absolute inset-0 flex items-center">
+                            <div className="w-full border-t border-gray-300" />
+                        </div>
+                        {/* NOme no centro da linha */}
+                        <div className="relative flex justify-center text-sm">
+                            <span className="bg-white px-2 text-gray-500">Ou continue com</span>
+                        </div>
+                    </div>
+                    
+                    <div className="mt-6 fleex gap-2">
+                        <AuthSocialButton icon={BsGoogle} onClick={()=>socialAction('google')}/>
+                    </div>
+
+                </div>
+                <div className="flex gap-2 justify-center text-sm mt-6 px-2 text-gray-500">
+                    <div>
+                        {typeForm === 'LOGIN'?'Novo por Aqui?': 'Já possui uma conta?'}
+                    </div>
+                    <div onClick={toggleTypeForm} className="underline cursor-pointer">
+                        {typeForm === 'LOGIN'?'Crie uma conta': 'Login'}
+                    </div>
+
+                </div>
+
+
+
 
             </div>
         </div>
